17f22b6b5ea01013b426e33ee64ef6f49a7e4275
Roberto Ye <huabox@gmail.com>
2013-10-26 09:56:11 +0800

    增加获取OracleMetaData的方法

---
+++ OraMetaData.Test/SourceUtilityTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+using NUnit.Framework;
+
+using OraMetaData;
+
+namespace OraMetaData
+{
+    [TestFixture]
+    public class SourceUtilityTest
+    {
+        [Test]
+        public void GetTableDefinition()
+        {
+            string strOcn = "DATA SOURCE=localora;PERSIST SECURITY INFO=True;USER ID=SCOTT;PASSWORD=tiger";
+            DataTable dt = SourceUtility.GetTableDefinition(strOcn, "");
+            int expert = 0;
+            Assert.Greater(dt.Rows.Count, expert);
+        }
+    }
+}
---
+++ OraMetaData/SourceUtility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Oracle.DataAccess.Client;
+
+
+namespace OraMetaData
+{
+	public class SourceUtility
+	{
+		public static DataTable GetTableDefinition(string strConn, string strFilter)
+		{
+			using (OracleConnection ocn = new OracleConnection(strConn))
+			{
+				ocn.Open();
+				string sqlFMT = @"SELECT * FROM DEPT    
+									{0} {1}
+									ORDER BY DEPTNO";
+				string sql = string.Format(sqlFMT, string.IsNullOrEmpty(strFilter) ? "" : "WHERE",
+								string.IsNullOrEmpty(strFilter) ? "" : strFilter);                
+				OracleCommand ocmd = new OracleCommand(sql, ocn);
+				DataSet ds = new DataSet();
+				OracleDataAdapter adapter = new OracleDataAdapter(ocmd);
+				adapter.Fill(ds);
+				return ds.Tables[0];
+			}    
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="strConn"></param>
+		/// <param name="pName"></param>
+		/// <param name="prs"></param>
+		/// <returns></returns>
+		public static DataSet GetDataSet(string strConn, string pName,OracleParameterCollection prs)
+		{
+			using (OracleConnection ocn = new OracleConnection(strConn))
+			{
+				ocn.Open();
+				OracleCommand ocmd = new OracleCommand(pName, ocn);
+				ocmd.CommandType = CommandType.StoredProcedure;
+                OracleParameter pra = new OracleParameter();
+
+				ocmd.Parameters.Add(par);
+
+				DataSet ds = new DataSet();
+				OracleDataAdapter adapter = new OracleDataAdapter(ocmd);
+				adapter.Fill(ds);
+				return ds;
+			}
+		}
+	}
+}
